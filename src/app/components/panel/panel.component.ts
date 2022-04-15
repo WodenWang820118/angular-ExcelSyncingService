74f7c400e65b0f85cdceaf8e3f3be96a7fe16be7
MOLDEX3D\alexwang <alex0972112871@gmail.com>
2022-04-15 10:48:14 +0800

    upload function; types setting; required package

--- src/app/components/panel/panel.component.ts
+++ src/app/components/panel/panel.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as d3 from 'd3';
+import * as Excel from 'exceljs'
 
 @Component({
   selector: 'app-panel',
@@ -11,5 +11,32 @@ export class PanelComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  async parseXLSX(event: any) {
+    const fileRes = event.currentTarget.files[0];
+    const buffer = await this.readFile(fileRes);
+    console.log(buffer);
+    this.generateExcelWorkbook(buffer);
+    
+  }
+
+  readFile(fileRes: any) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsArrayBuffer(fileRes);
+      reader.onload = () => {
+        resolve(reader.result);
+      }
+    })
+  }
+
+  async generateExcelWorkbook(buffer: any) {
+    const workbook = new Excel.Workbook()
+    const file = await workbook.xlsx.load(buffer as Buffer);
+    const sheet = file.getWorksheet("sheet1");
+    sheet.eachRow((row, rowNumber) => {
+      console.log(row.values)
+    })
   }
 }
